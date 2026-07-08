e0e36f52e7acf594980f039c79d55f96db108212
shayan gowda <shayangowda17@gmail.com>
2026-07-08 16:13:15 +0530

    Initial project structure

---
+++ backend/src/main/java/com/resolveone/ResolveOneApplication.java
@@ -0,0 +1,13 @@
+package com.resolveone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResolveOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResolveOneApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/resolveone/ResolveOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resolveone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResolveOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
